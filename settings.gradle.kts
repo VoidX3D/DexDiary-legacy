2677897ae45f841689a22baeed67fd5686eeb0cb
VoidX3D <PLAYZSPRESTON2@GMAIL.COM>
2026-04-13 12:14:54 +0545

    Complete Dex Diary project expansion and GitHub Actions setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,16 @@
-rootProject.name = "DexDiary"
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Dex Diary"
 include(":app")
